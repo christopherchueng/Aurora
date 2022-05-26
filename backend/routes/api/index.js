b210bdf86f78f9a39b1e52ba13ea5baedf2db76b
christopherchueng <christopherchueng@gmail.com>
2022-05-26 15:39:09 -0400

    User Login Route

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,36 +1,13 @@
 const router = require('express').Router();
+const sessionRouter = require('./session.js');
+const usersRouter = require('./users.js');
 
-const asyncHandler = require('express-async-handler');
-const { setTokenCookie } = require('../../utils/auth.js');
+router.use('/session', sessionRouter);
 
-// // TEST GET /api/require-auth
-// const { requireAuth } = require('../../utils/auth.js');
-// router.get('/require-auth', requireAuth, (req, res) => {
-    //     return res.json(req.user);
-    //   }
-    // );
-
-    // // TEST GET /api/restore-user
-    // const { restoreUser } = require('../../utils/auth.js');
-    // router.get('/restore-user', restoreUser, (req, res) => {
-        //     return res.json(req.user);
-        //   }
-        // );
-
-// // test user auth
-// const { User } = require('../../db/models');
-// router.get('/set-token-cookie', asyncHandler(async (_req, res) => {
-//   const user = await User.findOne({
-//       where: {
-//         username: 'Demo-lition'
-//       }
-//     });
-//   setTokenCookie(res, user);
-//   return res.json({ user });
-// }));
+router.use('/users', usersRouter);
 
 // router test
-router.post('/test', function(req, res) {
+router.post('/test', (req, res) => {
     res.json({ requestBody: req.body });
 });
 
