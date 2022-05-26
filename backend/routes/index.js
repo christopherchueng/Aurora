54a4066cdb65dbd36ca9ae8de20b3342653ea6ff
christopherchueng <christopherchueng@gmail.com>
2022-05-26 13:31:11 -0400

    Created API router

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
+const apiRouter = require('./api');
+
+// access all api routes.
+router.use('/api', apiRouter);
+
 // test route. change later
 router.get('/hello/world', function(req, res) {
   res.cookie('XSRF-TOKEN', req.csrfToken());
