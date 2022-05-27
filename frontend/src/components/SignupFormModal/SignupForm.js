6861a92ad4f16c39df233d415fe082cfd00ceec4
christopherchueng <christopherchueng@gmail.com>
2022-05-27 15:51:07 -0400

    Sign up Modal completed

--- frontend/src/components/SignupFormModal/SignupForm.js
+++ frontend/src/components/SignupFormModal/SignupForm.js
@@ -1,10 +1,9 @@
 import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { Redirect } from "react-router-dom";
 import * as sessionActions from "../../store/session";
 import './SignupForm.css';
 
-function SignupFormPage() {
+function SignupForm() {
   const dispatch = useDispatch();
   const sessionUser = useSelector((state) => state.session.user);
   const [email, setEmail] = useState("");
@@ -12,8 +11,6 @@ function SignupFormPage() {
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
   const [errors, setErrors] = useState([]);
-
-  if (sessionUser) return <Redirect to="/" />;
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -74,4 +71,4 @@ function SignupFormPage() {
   );
 }
 
-export default SignupFormPage;
+export default SignupForm;
---
+++ frontend/src/components/SignupFormModal/index.js
@@ -0,0 +1,20 @@
+import React, { useState } from "react";
+import SignupForm from "./SignupForm";
+import { Modal } from '../../context/Modal'
+
+function SignupFormModal() {
+  const [showModal, setShowModal] = useState(false);
+
+  return (
+    <>
+      <button onClick={() => setShowModal(true)}>Sign Up</button>
+      {showModal && (
+        <Modal onClose={() => setShowModal(false)}>
+          <SignupForm />
+        </Modal>
+      )}
+    </>
+  )
+}
+
+export default SignupFormModal;
