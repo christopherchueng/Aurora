da67216c8ce7bc181d7371e97c2951f316d6c374
christopherchueng <christopherchueng@gmail.com>
2022-05-28 15:03:31 -0400

    Added more user seeders

--- backend/db/seeders/20220526173925-demo-user.js
+++ backend/db/seeders/20220526173925-demo-user.js
@@ -18,14 +18,36 @@ module.exports = {
         email: 'user2@user.io',
         username: 'FakeUser2',
         hashedPassword: bcrypt.hashSync('password3')
-      }
+      },
+      {
+        email: 'user3@user.io',
+        username: 'FakeUser3',
+        hashedPassword: bcrypt.hashSync('password4')
+      },
+      {
+        email: 'user4@user.io',
+        username: 'FakeUser4',
+        hashedPassword: bcrypt.hashSync('password5')
+      },
+      {
+        email: 'user5@user.io',
+        username: 'FakeUser5',
+        hashedPassword: bcrypt.hashSync('password6')
+      },
     ], {});
   },
 
   down: (queryInterface, Sequelize) => {
     const Op = Sequelize.Op;
     return queryInterface.bulkDelete('Users', {
-      username: { [Op.in]: ['Demo-lition', 'FakeUser1', 'FakeUser2'] }
+      username: { [Op.in]: [
+        'Demo-lition',
+        'FakeUser1',
+        'FakeUser2',
+        'FakeUser3',
+        'FakeUser4',
+        'FakeUser5'
+      ]}
     }, {});
   }
 };
