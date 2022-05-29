22dc99a38df9858c028700532119da0b8950767e
christopherchueng <christopherchueng@gmail.com>
2022-05-29 11:24:58 -0400

    Created first selection field div

--- frontend/src/components/DiscoverPage/index.js
+++ frontend/src/components/DiscoverPage/index.js
@@ -5,7 +5,19 @@ const DiscoverPage = () => {
     return (
         <div className='discover-ctn'>
             <h1>Here in Discover</h1>
-
+            <div className='selection-field'>
+                <div className='selection-field-title'>
+                    <h2>Curated for you</h2>
+                </div>
+                <div className='selection-field-tracks'>
+                    <ul>
+                        <li>Track 1</li>
+                        <li>Track 2</li>
+                        <li>Track 3</li>
+                        <li>Track 4</li>
+                    </ul>
+                </div>
+            </div>
         </div>
     );
 }
