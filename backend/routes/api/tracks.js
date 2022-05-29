9d40bcd1f968917ffc00f89aaa34954114283dc8
christopherchueng <christopherchueng@gmail.com>
2022-05-28 21:03:34 -0400

    Tracks Route set up with middleware and imports

--- backend/routes/api/tracks.js
+++ backend/routes/api/tracks.js
@@ -1,5 +1,13 @@
 const express = require('express');
+const { Tracks } = require('../../db/models');
+const { check } = require('express-validator');
+
+const { requireAuth } = require('../../utils/auth');
+const asyncHandler = require('express-async-handler');
+const { handleValidationErrors } = require('../../utils/validation');
 
 const router = express.Router();
+
+
 
 module.exports = router;
