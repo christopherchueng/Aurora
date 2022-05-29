17eb52fefc20044b240f8ce852f1d3eb91d23570
christopherchueng <christopherchueng@gmail.com>
2022-05-28 21:34:26 -0400

    Created Post route

--- backend/routes/api/tracks.js
+++ backend/routes/api/tracks.js
@@ -8,6 +8,25 @@ const { handleValidationErrors } = require('../../utils/validation');
 
 const router = express.Router();
 
+const trackValidators = [
+    check('title')
+        .exists({ checkFalsy: true })
+        .isLength({ max: 100 })
+        .withMessage('Please provide a title that is no more than 100 characters long.'),
+    check('genre')
+        .exists({ checkFalsy: true })
+        .withMessage('Please select a genre.'),
+    check('trackPath')
+        .exists({ checkFalsy: true })
+        .withMessage('Please provide a track.')
+        .isURL({ protocols: false }),
+    handleValidationErrors
+];
 
+router.post('/', requireAuth, trackValidators, asyncHandler(async (req, res) => {
+    const track = await Tracks.create(req.body)
+
+    return res.json(track);
+}))
 
 module.exports = router;
