49674e4a58882bde75efd9959fcd9e2fa1794345
christopherchueng <christopherchueng@gmail.com>
2022-05-30 13:03:06 -0400

    Dispatched all tracks to discover page

--- frontend/src/components/DiscoverPage/index.js
+++ frontend/src/components/DiscoverPage/index.js
@@ -1,7 +1,17 @@
 import { useState, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { NavLink } from "react-router-dom";
+import { getTracks } from "../../store/trackReducer";
 import './DiscoverPage.css'
 
 const DiscoverPage = () => {
+    const dispatch = useDispatch();
+    const tracks = useSelector(state => state.track.entries)
+
+    useEffect(() => {
+        dispatch(getTracks())
+    }, [dispatch]);
+
     return (
         <div className='discover-ctn'>
             <h1>Here in Discover</h1>
@@ -11,10 +21,13 @@ const DiscoverPage = () => {
                 </div>
                 <div className='selection-field-tracks'>
                     <ul>
-                        <li>Track 1</li>
-                        <li>Track 2</li>
-                        <li>Track 3</li>
-                        <li>Track 4</li>
+                        {Object.values(tracks).map(({ id, imagePath }) => (
+                            <li key={id}>
+                                <NavLink to={`/tracks/${id}`}>
+                                    <img src={imagePath}></img>
+                                </NavLink>
+                            </li>
+                        ))}
                     </ul>
                 </div>
             </div>
