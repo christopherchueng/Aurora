706c4a25de60acd7faa28f387b53a95483e1e3ef
christopherchueng <christopherchueng@gmail.com>
2022-06-02 14:02:43 -0400

    Cleaned up TrackIdPage with just regular JSX without edit

--- frontend/src/components/Tracks/TrackIdPage.js
+++ frontend/src/components/Tracks/TrackIdPage.js
@@ -82,52 +82,29 @@ const TrackIdPage = ({tracks}) => {
     return (
         <>
             <div className='music-player-ctn'>
-                {/* <form onSubmit={handleSubmit}> */}
+                {/* IF THE EDIT BUTTON IS PRESSED, BRING UP FORM */}
+                openEdit ?
+                    <UpdateTrackForm /> :
+                    // START OF NO EDIT FORM
                     <div className='music-player-content'>
                         <div className='track-bar'>
-                            {/* <form onSubmit={handleSubmit}> */}
-
-                                {/* ------------------ IMAGEPATH ------------------ */}
-                                <div className='cover-photo-ctn'>
-                                    {/* If edit button is clicked, form will appear.
-                                    Otherwise, display cover photo */}
-                                    {openEdit ?
-                                    <input
-                                        name='imagePath'
-                                        type='text'
-                                        alt='https://aurora-tracks.s3.amazonaws.com/Aurora-Tracks/default-imagePath.png'
-                                        value={imagePath}
-                                        placeholder='Insert an image link'
-                                        onChange={e => setImagePath(e.target.value)}
-                                    /> :
-                                    (saveChanges && <img className='cover-photo' src={track?.imagePath}></img>)}
-                                </div>
-                            {/* </form> */}
+                            {/* ------------------ IMAGEPATH ------------------ */}
+                            <div className='cover-photo-ctn'>
+                                {/* If edit button is clicked, form will appear.
+                                Otherwise, display cover photo */}
+                                {saveChanges && <img className='cover-photo' src={track?.imagePath}></img>)}
+                            </div>
                         </div>
+                        {/* START MEDIA CONTROLS */}
                         <div className='media-controls'>
                             <div className='control-left'>
-                                {/* <form onSubmit={handleSubmit}> */}
 
-                                    {/* ------------------ TITLE ------------------ */}
-                                    <div className='track-title'>
-                                        <h1>
-                                            {/* If edit button is clicked, form will appear.
-                                            Otherwise, display title */}
-                                            {openEdit ?
-                                                <input
-                                                    type='text'
-                                                    aria-label='Title'
-                                                    value={title}
-                                                    onChange={e => setTitle(e.target.value)}
-                                                /> :
-                                                (track?.title)
-                                            }
-                                        </h1>
-                                    </div>
-                                    <div>
-                                        {openEdit && <ErrorMessage error={errors.title} />}
-                                    </div>
-                                {/* </form> */}
+                                {/* ------------------ TITLE ------------------ */}
+                                <div className='track-title'>
+                                    <h1>
+                                        {track?.title}
+                                    </h1>
+                                </div>
 
                                 {/* ------------------ ARTIST ------------------ */}
                                 <div className='track-artist'>
@@ -136,9 +113,15 @@ const TrackIdPage = ({tracks}) => {
                             </div>
 
                             {/* ------------------ MEDIA CONTROLS ------------------ */}
+                            {/* START MEDIA CONTROLS */}
                             <div className='control-center'>
                                 <div className='back-ctn'>
-                                    <button type='button' className='back'><i className="fa-solid fa-backward-step fa-3x"></i></button>
+                                    <button
+                                        type='button'
+                                        className='back'
+                                    >
+                                        <i className="fa-solid fa-backward-step fa-3x"></i>
+                                    </button>
                                 </div>
                                 <div className='play-ctn'>
                                     {/* If not playing, play button will display */}
@@ -171,101 +154,55 @@ const TrackIdPage = ({tracks}) => {
                                     </button>
                                 </div>
 
-                            </div>
+                            {/* ------------------ VOLUME ------------------ */}
                             <div className='volume-ctn'>
                                 Volume line here
                             </div>
+                        {/* END MEDIA CONTROLS */}
                         </div>
                     </div>
-                <div className='adjustment-ctn'>
-                    {/* <form onSubmit={handleSubmit}> */}
 
-                        {/* ------------------ EDIT AND SAVE CHANGES BUTTONS ------------------ */}
+                    {/* START EDIT/DELETE */}
+                    <div className='edit-save-ctn'>
+                        {/* ------------------ EDIT AND DELETE BUTTONS ------------------ */}
                         <div className='edit-ctn'>
                             {/* If edit button is clicked, openEdit will be set to true and
                             will display save changes button. This will allow form to appear.
                             Conversely, if edit button is not clicked, the edit button will be displayed.
                             Edit button has a 'submit' type because when openEdit is false, that means we are NOT
                             making any changes. Thus, the edits are made and locked in.*/}
-                            {openEdit ?
-                                (<button
-                                    type='button'
-                                    className='saveChanges'
-                                    // When Save Changes button is CLICKED, Editing will NOT be allowed and
-                                    // Edit button WILL BE DISPLAYED.
-                                    onClick={() => setOpenEdit(false) &&
-                                    setSaveChanges(false)}>
-                                        Save Changes
-                                </button>) :
-                                (<button
-                                    type='submit'
-                                    className='inline-edit-Track'
-                                    // When Edit button is CLICKED, Editing will be allowed and Save Changes
-                                    // button WILL BE DISPLAYED.
-                                    onClick={() => setOpenEdit(true) &&
-                                    setSaveChanges(true)}>
-                                        Edit
-                                </button>)
-                            }
-                        </div>
-                    {/* </form> */}
+                            (<button
+                                type='submit'
+                                className='inline-edit-Track'
+                                // When Edit button is CLICKED, Editing will be allowed and Save Changes
+                                // button WILL BE DISPLAYED.
+                                onClick={() => setOpenEdit(true) &&
+                                setSaveChanges(true)}>
+                                    Edit
+                            </button>)
 
-                    {/* ------------------ DELETE ------------------ */}
-                    <div className='delete-ctn'>
-                        <DeleteTrackComponent trackId={trackId} />
+                            {/* ------------------ DELETE ------------------ */}
+                            <div className='delete-ctn'>
+                                    <DeleteTrackComponent trackId={trackId} />
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className='track-info-ctn'>
+                        {/* ------------------ DESCRIPTION ------------------ */}
+                        <div className='description'>
+                            {/* If edit button is clicked, form will appear.
+                            Otherwise, display description */}
+                            {saveChanges && track?.description}
+                        </div>
+                        {/* ------------------ GENRE ------------------ */}
+                        <div className='genre-ctn'>
+                            <div className='genre'>
+                                <span>{saveChanges && track?.genre}</span>
+                            </div>
+                        </div>
                     </div>
                 </div>
-                    <div className='track-info-ctn'>
-                        {/* <form onSubmit={handleSubmit}> */}
-
-                            {/* ------------------ DESCRIPTION ------------------ */}
-                            <div className='description'>
-                                {/* If edit button is clicked, form will appear.
-                                Otherwise, display description */}
-                                {openEdit ?
-                                    (<textarea
-                                        name='description'
-                                        value={description}
-                                        placeholder='Description'
-                                        onChange={e => setDescription(e.target.value)}
-                                    />) :
-                                    (saveChanges && track?.description)
-                                }
-                            </div>
-                            <div>
-                                {saveChanges && <ErrorMessage error={errors.description} />}
-                            </div>
-                        {/* </form> */}
-
-                            {/* ------------------ GENRE ------------------ */}
-                            <div className='genre-ctn'>
-                                {/* <form onSubmit={handleSubmit}> */}
-                                    <div className='genre'>
-                                        {/* If edit button is clicked, form will appear.
-                                        Otherwise, display genre dropdown */}
-                                        {openEdit ?
-                                            (<select
-                                                type='text'
-                                                aria-label='Title'
-                                                value={genre}
-                                                onChange={e => setGenre(e.target.value)}
-                                            >
-                                                {genres.map(genre => (
-                                                    <option key={genre}>{genre}</option>
-                                                ))}
-                                            </select>) :
-                                            <span>{saveChanges && track?.genre}</span>
-                                        }
-                                    </div>
-                                    <div>
-                                        {openEdit && <ErrorMessage error={errors.genre} />}
-                                    </div>
-                                {/* </form> */}
-                            </div>
-                        {/* </form> */}
-                    </div>
-                {/* </form> */}
-            </div>
                 <div className='comment-section-ctn'>
                     <h1>Comments down below</h1>
                 </div>
