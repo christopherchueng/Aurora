ecfd7febc3ed0cb8e1300b3c20c8197380f7388e
christopherchueng <christopherchueng@gmail.com>
2022-06-03 00:03:03 -0400

    Removed tracks index comments

--- frontend/src/components/Tracks/index.js
+++ frontend/src/components/Tracks/index.js
@@ -1,26 +1,14 @@
 import { useState, useEffect } from "react";
-import { Route, Switch, useParams } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import CreateTrackForm from "./CreateTrackForm";
 import UpdateTrackForm from "./UpdateTrackForm";
 import TrackIdPage from "./TrackIdPage";
-import { useDispatch, useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 import { useEditTrackContext } from "../../context/EditTrackContext";
 
 const Tracks = () => {
     const tracks = useSelector(state => state.track.entries)
-    // const [openEdit, setOpenEdit] = useState(false)
-    // const [saveChanges, setSaveChanges] = useState(true)
-    const { openEdit, setOpenEdit, saveChanges, setSaveChanges } = useEditTrackContext();
-
-    // // On mount, edit button is off.
-    // useEffect(() => {
-    //     setOpenEdit(false);
-    // }, [])
-
-    // // // On mount, changes are saved.
-    // useEffect(() => {
-    //     setSaveChanges(true);
-    // }, [])
+    const { openEdit } = useEditTrackContext();
 
     return (
         <>
@@ -32,14 +20,10 @@ const Tracks = () => {
                     {/* If edit button is clicked: 2 options:
                     If save changes is clicked, Update the track. Otherwise, */}
                     {openEdit
-                        // ? saveChanges
                         ? <UpdateTrackForm tracks={tracks} />
-                            // : setOpenEdit(false) && setSaveChanges(true)
                         : <TrackIdPage tracks={tracks} />
-
                     }
                 </Route>
-
             </Switch>
         </>
     )
