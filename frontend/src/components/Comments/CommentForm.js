29db500d08b41f7f9949a66952bccba6b9f4eb6d
christopherchueng <christopherchueng@gmail.com>
2022-06-03 17:31:51 -0400

    Now able to display buttons when textbox is in focus and hidden when not

--- frontend/src/components/Comments/CommentForm.js
+++ frontend/src/components/Comments/CommentForm.js
@@ -10,6 +10,28 @@ const CommentForm = ({trackId, user}) => {
 
     // States
     const [message, setMessage] = useState('')
+    const [boxClicked, setBoxClicked] = useState(false)
+
+    const showPostBtn = () => {
+        const prevVal = boxClicked; // Box is closed
+        setBoxClicked(!prevVal) // Set box to open
+        if (prevVal) { // if Box is open
+            setBoxClicked(prevVal) // set box to open/true
+        }
+        else {
+            setBoxClicked(!prevVal) // Otherwise, box is closed
+        }
+    }
+
+    const hidePostBtn = () => {
+        const prevVal = boxClicked; // Box is closed
+        setBoxClicked(prevVal) // set box to closed
+        if (!prevVal) { // If box is NOT closed (open),
+            setBoxClicked(prevVal) // set box as open/true
+        } else {
+            setBoxClicked(!prevVal) // Otherwise, keep box closed
+        }
+    }
 
     const onSubmit = async (e) => {
         e.preventDefault()
@@ -22,21 +44,45 @@ const CommentForm = ({trackId, user}) => {
 
         await dispatch(postComment(payload))
         setMessage('')
+        setBoxClicked(false)
         history.push(`/tracks/${trackId}`)
     }
     return (
-        <div className='comment-textbox'>
-            <form onSubmit={onSubmit}>
-                <textarea
-                    name='comment'
-                    value={message}
-                    placeholder='Add a comment'
-                    onChange={e => setMessage(e.target.value)}
-                />
-                <button>Add Comment</button>
-            </form>
+        <>
+            <div className='comment-textbox'>
+                <form onSubmit={onSubmit}>
+                    <div>
+                        <textarea
+                            name='comment'
+                            value={message}
+                            placeholder='Add a comment'
+                            onChange={e => setMessage(e.target.value)}
+                            onClick={showPostBtn}
+                            />
+                            {boxClicked
+                            ?
+                                <>
+                                    <button
+                                        type='submit'
+                                        disabled={!message}
+                                        onClick={onSubmit}
+                                    >
+                                        Add Comment
+                                    </button>
+                                    <button
+                                        type='button'
+                                        onClick={hidePostBtn}
+                                    >
+                                        Cancel
+                                    </button>
+                                </>
+                            :   ''}
+                    </div>
+                </form>
 
-        </div>
+            </div>
+
+        </>
     )
 }
 
