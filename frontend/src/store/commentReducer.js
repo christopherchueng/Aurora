45883802969b4e23356c9f96fc4b66cef5fab2c8
christopherchueng <christopherchueng@gmail.com>
2022-06-03 13:27:58 -0400

    Created action creator and thunk for get Comments

--- frontend/src/store/commentReducer.js
+++ frontend/src/store/commentReducer.js
@@ -1,5 +1,24 @@
 import { csrfFetch } from "./csrf";
 
+const LOAD_COMMENTS = 'comment/loadComments';
+
+// Action creators
+export const loadComments = (comments) => {
+    return {
+        type: LOAD_COMMENTS,
+        comments
+    }
+}
+
+export const getComments = (track) => async (dispatch) => {
+    const response = await csrfFetch('api/comments', {
+        body: JSON.stringify(track)
+    })
+
+    const comments = await response.json();
+    dispatch(loadComments(comments))
+}
+
 const initialState = { entries: {}, isLoading: true };
 
 const commentReducer = (state = initialState, action) => {
