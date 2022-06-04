790a56ce8196f7441209559bcdde18a1a178d01e
christopherchueng <christopherchueng@gmail.com>
2022-06-04 19:38:30 -0400

    Created delete comment component

---
+++ frontend/src/components/Comments/DeleteComment.js
@@ -0,0 +1,13 @@
+import { useDispatch, useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
+import { useHistory } from "react-router-dom";
+import { useState } from "react";
+
+
+const DeleteComment = () => {
+    return (
+        <h1>Hi from DELETE</h1>
+    )
+}
+
+export default DeleteComment;
