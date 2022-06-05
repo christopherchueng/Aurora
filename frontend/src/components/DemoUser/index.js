59d7974836e36cb8c35c6e49c001067e1fa478c3
christopherchueng <christopherchueng@gmail.com>
2022-06-05 09:33:28 -0400

    Demo User comp created

---
+++ frontend/src/components/DemoUser/index.js
@@ -0,0 +1,20 @@
+import { useDispatch } from "react-redux";
+import * as sessionActions from '../../store/session'
+
+const DemoUser = () => {
+    const dispatch = useDispatch();
+
+    const demoClick = async (e) => {
+        e.preventDefault();
+
+        const credential = 'Demo-lition';
+        const password = 'password';
+        const demo = await dispatch(sessionActions.login({ credential, password }))
+        return demo;
+    }
+
+    return (
+        <button className='demo-btn' onClick={demoClick}>Demo</button>
+
+    )
+}
