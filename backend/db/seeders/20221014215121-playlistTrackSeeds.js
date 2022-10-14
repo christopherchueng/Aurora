d78ddd3359bd1dd9520fd00d1358fe9a6a6f79f2
christopherchueng <christopherchueng@gmail.com>
2022-10-14 18:09:51 -0400

    Created playlistTracks seed data join table

--- backend/db/seeders/20221014215121-playlistTrackSeeds.js
+++ backend/db/seeders/20221014215121-playlistTrackSeeds.js
@@ -7,11 +7,129 @@ module.exports = {
       Return a promise to correctly handle asynchronicity.
 
       Example:
-      return queryInterface.bulkInsert('People', [{
-        name: 'John Doe',
-        isBetaMember: false
-      }], {});
-    */
+      */
+    return queryInterface.bulkInsert('PlaylistTracks', [
+      {
+        playlistId: 1,
+        trackId: 2,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        playlistId: 1,
+        trackId: 3,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        playlistId: 1,
+        trackId: 8,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        playlistId: 1,
+        trackId: 9,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        playlistId: 1,
+        trackId: 11,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        playlistId: 2,
+        trackId: 18,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        playlistId: 2,
+        trackId: 14,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        playlistId: 2,
+        trackId: 16,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        playlistId: 3,
+        trackId: 4,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        playlistId: 3,
+        trackId: 1,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        playlistId: 3,
+        trackId: 7,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        playlistId: 4,
+        trackId: 15,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        playlistId: 4,
+        trackId: 17,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        playlistId: 5,
+        trackId: 17,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        playlistId: 5,
+        trackId: 6,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        playlistId: 5,
+        trackId: 1,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        playlistId: 5,
+        trackId: 8,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        playlistId: 5,
+        trackId: 9,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        playlistId: 5,
+        trackId: 13,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        playlistId: 5,
+        trackId: 4,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+    ], {});
   },
 
   down: (queryInterface, Sequelize) => {
@@ -20,7 +138,7 @@ module.exports = {
       Return a promise to correctly handle asynchronicity.
 
       Example:
-      return queryInterface.bulkDelete('People', null, {});
-    */
+      */
+    return queryInterface.bulkDelete('PlaylistTracks', null, {});
   }
 };
