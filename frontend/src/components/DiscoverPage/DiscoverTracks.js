11f5d267b7e2c4bc13867d5ac7035ebf84c84f59
christopherchueng <christopherchueng@gmail.com>
2022-10-19 20:39:54 -0400

    Play pause button appears when mouse hovers over picture

--- frontend/src/components/DiscoverPage/DiscoverTracks.js
+++ frontend/src/components/DiscoverPage/DiscoverTracks.js
@@ -5,6 +5,7 @@ import { Link } from "react-router-dom";
 const DiscoverTracks = ({track}) => {
     const playing = useSelector(state => state?.mediaControl?.playing)
     const [isPlaying, setIsPlaying] = useState(!!playing)
+    const [showButton, setShowButton] = useState(false)
 
     const audioPlayer = useRef()
 
@@ -24,17 +25,17 @@ const DiscoverTracks = ({track}) => {
                 <div className="playlistTrack-trackPath">
                     <audio src={track.trackPath} ref={audioPlayer}></audio>
                 </div>
-                <div className="playlistTrack-imagePath">
+                <div className="playlistTrack-imagePath" onMouseEnter={() => setShowButton(true)} onMouseLeave={() => setShowButton(false)}>
                     <div className="play-playlistTrack-ctn">
                         <button
                             type='button'
                             className='play-pause-playlistTrack'
                             onClick={() => setIsPlaying(!isPlaying)}
                         >
-                            {isPlaying
+                            {showButton && (isPlaying
                             ? <i className="fa-solid fa-circle-pause fa-4x fontA-pause"></i>
                             : <i className="fa-solid fa-circle-play fa-4x fontA-play"></i>
-                            }
+                            )}
                         </button>
                     </div>
                     <Link to={`/tracks/${track.id}`}><img className='playlistTrack-cover-photo' src={track.imagePath}></img></Link>
